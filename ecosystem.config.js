a63bb28118d8df854f138beba7d1e0430a3b9566
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-02-21 11:59:35 +0300

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  apps : [{
-    name:"yom-kitchen",
-    script: 'npm start',
-    env: {
-      NODE_ENV: "production", 
-      "NEXT_PUBLIC_API_BASE_URL": "http://localhost:8080"
-  }
-  }],
+  apps: [
+    {
+      name: "yom-kitchen",
+      script: "npm start",
+      env: process.env,
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'root',
-      host : 'appeyetatechmobile.com',
-      ref  : 'origin/main',
-      repo : 'https://github.com/samuelabebayehu/yom-kitchen-frontend2.git',
-      path : '/root/yom/yom-frontend',
-      'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes'
-    }
-  }
+  deploy: {
+    production: {
+      user: "root",
+      host: "appeyetatechmobile.com",
+      ref: "origin/main",
+      repo: "https://github.com/samuelabebayehu/yom-kitchen-frontend2.git",
+      path: "/root/yom/yom-frontend",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
 };
