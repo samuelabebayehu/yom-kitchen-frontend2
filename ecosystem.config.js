a512b896ba5752a75ce95d13608a4d1817c0abd2
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-02-21 11:34:36 +0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user : 'root',
       host : 'appeyetatechmobile.com',
       ref  : 'origin/main',
-      repo : 'github.com/samuelabebayehu/yom-kitchen-frontend2.git',
+      repo : 'https://github.com/samuelabebayehu/yom-kitchen-frontend2.git',
       path : '/root/yom/yom-kitchen-frontend2',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
