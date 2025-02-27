02bc63515093b3f0ab3c6faee48a5720559ecf67
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-02-27 11:56:58 +0300

    fix mobile sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetDescription, SheetTitle } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -206,7 +206,9 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            <div className="flex h-full w-full flex-col">{children}</div>
+            <SheetTitle className="hidden"></SheetTitle> {/*👈🏻 */}
+            <SheetDescription className="hidden"></SheetDescription> {/*👈🏻 */}
+            <div className="flex h-full w-full flex-col bg-white">{children}</div>
           </SheetContent>
         </Sheet>
       )
